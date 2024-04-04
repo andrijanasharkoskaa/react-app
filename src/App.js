ab10cb48977f583065dc5da6dff96284a463d0f8
Andrijana Sharkoska <39841060+andrijanasharkoskaa@users.noreply.github.com>
2024-04-04 19:13:25 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Automatic Deployment is Set Up!
+         Automatic Deployment is Set Up! x 2
         </p>
         <a
           className="App-link"
